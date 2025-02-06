b4fde36f074a42c1f353f10df83184833c32119b
myeongje <tiqmtiqm6541@gmail.com>
2025-02-06 20:15:03 +0900

    init commit

---
+++ src/main/java/org/example/scheduledevelop/ScheduledevelopApplication.java
@@ -0,0 +1,13 @@
+package org.example.scheduledevelop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduledevelopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduledevelopApplication.class, args);
+    }
+
+}
